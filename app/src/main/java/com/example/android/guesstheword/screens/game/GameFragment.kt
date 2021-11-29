dbc5e55ba935a9154c1aff8ecbdba50aa5758843
Fast050 <dragonman050@gmail.com>
2021-11-29 18:23:08 +0400

    practice concept : liveDate (life awareness) ,  encapsulation(liveData-MutableLiveData) , event-state

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,15 +17,19 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * Fragment where the game is played
@@ -36,51 +40,99 @@ class GameFragment : Fragment() {
 
     private lateinit var binding: GameFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.game_fragment,
-                container,
-                false
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
         )
 
         // Get the viewmodel
+        Log.i("GameFragment", "Called ViewModelProvider")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+//        // TODO (03) Move over this initialization to the GameViewModel
+//        viewModel.resetList()
+//        viewModel.nextWord()
+
+        // TODO (04) Update these onClickListeners to refer to call methods in the ViewModel then
+        // update the UI
+
+        viewModel.score.observe(viewLifecycleOwner, Observer {
+
+            updateScoreText()
+
+        })
+
+        viewModel.word.observe(viewLifecycleOwner, Observer {
+            updateWordText()
+        })
+
+
 
         binding.correctButton.setOnClickListener {
             viewModel.onCorrect()
-            updateScoreText()
-            updateWordText()
+
         }
         binding.skipButton.setOnClickListener {
             viewModel.onSkip()
-            updateScoreText()
-            updateWordText()
+
         }
-        updateScoreText()
-        updateWordText()
+
+        viewModel.eventGameFinished.observe(viewLifecycleOwner, Observer {
+
+            if (it)
+            {
+                gameFinished()
+                viewModel.onGameFinishedCompleted()
+            }
+
+
+        })
+
+
         return binding.root
 
     }
 
+    // TODO (02) Move over methods resetList, nextWord, onSkip and onCorrect to the GameViewModel
+    /**
+     * Resets the list of words and randomizes the order
+     */
+
+
     /**
      * Called when the game is finished
      */
-    fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
-        findNavController(this).navigate(action)
+    private fun gameFinished() {
+//        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
+//        findNavController(this).navigate(action)
+        Snackbar.make(requireView(),"event happen ",Snackbar.LENGTH_SHORT).show()
     }
+
+    /**
+     * Moves to the next word in the list
+     */
+
+
+    /** Methods for buttons presses **/
+
 
     /** Methods for updating the UI **/
 
+    // TODO (05) Update these methods to get word and score from the viewmodel
     private fun updateWordText() {
-        binding.wordText.text = viewModel.word
+        binding.wordText.text = viewModel.word.value.toString()
+
     }
 
     private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
+        binding.scoreText.text = viewModel.score.value.toString()
     }
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,6 +16,8 @@
 
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
@@ -25,10 +27,22 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+    get() = _word
+
+
 
     // The current score
-    var score = 0
+    private val _score= MutableLiveData<Int>()
+    val score : LiveData<Int>
+    get()=_score
+
+    // The game finished event (true :happen , false :not happen)
+    private val _eventGameFinished= MutableLiveData<Boolean>()
+    val eventGameFinished:LiveData<Boolean>
+    get() = _eventGameFinished
+
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -37,12 +51,15 @@ class GameViewModel : ViewModel() {
     init {
         resetList()
         nextWord()
+        _score.value=0
+        _word.value=""
+        _eventGameFinished.value=false
     }
 
     /**
      * Resets the list of words and randomizes the order
      */
-    private fun resetList() {
+    fun resetList() {
         wordList = mutableListOf(
                 "queen",
                 "hospital",
@@ -72,24 +89,30 @@ class GameViewModel : ViewModel() {
     /**
      * Moves to the next word in the list
      */
-    private fun nextWord() {
+    fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
             // gameFinished() should happen here
+            _eventGameFinished.value=true
         } else {
-            word = wordList.removeAt(0)
+            _word.value=wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        _score.value=_score.value?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value=_score.value?.plus(1)
         nextWord()
+    }
+
+    fun onGameFinishedCompleted()
+    {
+        _eventGameFinished.value=false
     }
 }
